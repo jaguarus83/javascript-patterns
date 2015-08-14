d15fbffe4ae86965532a3e4c36b881c883e4936c
Andrew Ryabinin <andrew.ryabinin@aofl.com>
2015-08-14 12:46:12 +0300

    sorting with merge

--- sorting_with_merge.js
+++ sorting_with_merge.js
@@ -1,5 +1,15 @@
 function merge(left, right) {
+  var result = [];
 
+  while (left.length > 0 && right.length > 0) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  return result.concat(left).concat(right);
 }
 
 function mergeSort(items) {
@@ -9,7 +19,13 @@ function mergeSort(items) {
 
   var middle = Math.floor(items.length / 2),
     left = items.slice(0, middle),
-    right = items.slice(0);
+    right = items.slice(middle);
 
   return merge(mergeSort(left), mergeSort(right));
 }
+
+exports.merge = merge;
+exports.mergeSort = mergeSort;
+
+
+
---
+++ tests/sorting-test.js
@@ -0,0 +1,5 @@
+var sorting = require('../sorting_with_merge');
+
+var sorted = sorting.mergeSort([8, 10, 2, 1, 6, 2, 13, 4, 9, 0])
+
+console.log(sorted);
